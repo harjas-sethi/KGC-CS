c94743d9310976e0f5755d222be3787cc6a34653
Harjas Sethi <55926548+harjas-sethi@users.noreply.github.com>
2023-04-22 10:18:13 -0700

    Add files via upload

---
+++ GamingAppLauncherApplication.java
@@ -0,0 +1,27 @@
+package com.in28minutes.learnspringframework.game;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan;
+ 
+import com.in28minutes.learnspringframework.game.GameRunner;
+import com.in28minutes.learnspringframework.game.GamingConsole;
+
+@Configuration
+@ComponentScan("com.in28minutes.learnspringframework.game")
+public class GamingAppLauncherApplication {
+	
+
+	public static void main(String[] args) {
+
+		try (var context = 
+				new AnnotationConfigApplicationContext
+					(GamingAppLauncherApplication.class)) {
+
+			context.getBean(GamingConsole.class).up();
+			
+			context.getBean(GameRunner.class).run();
+
+		}
+	}
+}
